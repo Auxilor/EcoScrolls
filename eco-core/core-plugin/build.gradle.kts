5d9723778464515a30802266c8a022762eb61404
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-04-04 23:04:54 +0100

    Modified publishing logic

--- eco-core/core-plugin/build.gradle.kts
+++ eco-core/core-plugin/build.gradle.kts
@@ -15,18 +15,7 @@ publishing {
     publications {
         create<MavenPublication>("shadow") {
             from(components["java"])
-            artifactId = "EcoScrolls"
-        }
-    }
-
-    repositories {
-        maven {
-            name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/Auxilor/eco")
-            credentials {
-                username = System.getenv("GITHUB_ACTOR")
-                password = System.getenv("GITHUB_TOKEN")
-            }
+            artifactId = rootProject.name
         }
     }
 
