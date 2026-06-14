aa41abf2a3ea05d127e18462a79dbbcd894a3d72
Exanthiax <107284021+Exanthiax@users.noreply.github.com>
2026-06-14 09:40:25 +0100

    Add wiki v2 documentation metadata

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecoscrolls/libreforge/EffectInscribeItem.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecoscrolls/libreforge/EffectInscribeItem.kt
@@ -2,16 +2,30 @@ package com.willfp.ecoscrolls.libreforge
 
 import com.willfp.eco.core.config.interfaces.Config
 import com.willfp.ecoscrolls.scrolls.Scrolls
+import com.willfp.libreforge.ArgType
 import com.willfp.libreforge.NoCompileData
 import com.willfp.libreforge.arguments
 import com.willfp.libreforge.effects.Effect
 import com.willfp.libreforge.triggers.TriggerData
 
 object EffectInscribeItem : Effect<NoCompileData>("inscribe_item") {
+    override val description = "Inscribes the found item with the specified scroll, increasing its level by one."
+
+    override val categories = setOf("inventory")
+
+    override val additionalInfo = listOf(
+        "Requires a trigger that provides an item, such as inscribe or try_inscribe."
+    )
+
     override val isPermanent = false
 
     override val arguments = arguments {
-        require("scroll", "You must specify the scroll!")
+        require(
+            "scroll",
+            "You must specify the scroll!",
+            description = "The id of the scroll to inscribe onto the item.",
+            type = ArgType.STRING
+        )
     }
 
     override fun onTrigger(config: Config, data: TriggerData, compileData: NoCompileData): Boolean {
